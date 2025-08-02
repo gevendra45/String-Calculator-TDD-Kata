bfe0c8c4d7ec5cd05b762cf4c89f9ef1c29b9dc9
Gevendra Verma <Gevendra.Verma@indexengines.com>
2025-08-02 12:53:07 +0530

    created sum of 2 numbers using string that can take more than 2 numbers as an input for generating sum.

--- code/main.py
+++ code/main.py
@@ -6,9 +6,6 @@ def Add(numbers: str) -> int:
     # Split by comma
     parts = numbers.split(',')
 
-    if len(parts) > 2:
-            raise ValueError("Only up to two numbers are allowed")
-
     # Sum up to two numbers (gracefully handles 1 or 2 numbers)
     total = 0
     for part in parts:
@@ -20,4 +17,4 @@ def Add(numbers: str) -> int:
 print(Add(""))          # Output: 0
 print(Add("1"))         # Output: 1
 print(Add("1,2"))       # Output: 3
-print(Add("1,2,3,4"))   # Output: ValueError: Only up to two numbers are allowed
+print(Add("1,2,3,4"))   # Output: 10
